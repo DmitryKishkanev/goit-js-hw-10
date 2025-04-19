3c15f7aeddae590b2b5be8badfd9e805594c6bed
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-04-20 01:26:36 +0300

    добавил календарь

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,28 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import '../css/1-timer.css';
+
+const buttonStart = document.querySelector('[data-start]');
+
+let userSelectedDate = '';
+const defaultDate = new Date();
+
+const options = {
+  dateFormat: 'd-m-Y H:i',
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: defaultDate,
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < defaultDate) {
+      buttonStart.classList.remove('isActive');
+      return window.alert('Please choose a date in the future');
+    }
+    userSelectedDate = selectedDates[0];
+    buttonStart.classList.add('isActive');
+
+    console.log(userSelectedDate);
+  },
+};
+
+const fp = flatpickr('#datetime-picker', options);
