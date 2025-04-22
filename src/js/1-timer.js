7ebd71d13e53b32fc02bb694c698f991fc13c450
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-04-22 17:33:13 +0300

    при запуске таймера кнопка не активна

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,16 +5,18 @@ import 'izitoast/dist/css/iziToast.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../css/1-timer.css';
 
-const buttonStart = document.querySelector('[data-start]');
-const fieldDays = document.querySelector('[data-days]');
-const fieldHours = document.querySelector('[data-hours]');
-const fieldMinutes = document.querySelector('[data-minutes]');
-const fieldSeconds = document.querySelector('[data-seconds]');
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  fieldDays: document.querySelector('[data-days]'),
+  fieldHours: document.querySelector('[data-hours]'),
+  fieldMinutes: document.querySelector('[data-minutes]'),
+  fieldSeconds: document.querySelector('[data-seconds]'),
+};
 
-buttonStart.addEventListener('click', onClick);
-
+// Переменная для хранения выбранной даты
 let userSelectedDate = '';
 
+// Необязательный объект параметров функции flatpickr
 const options = {
   dateFormat: 'd-m-Y H:i',
   enableTime: true,
@@ -23,34 +25,48 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
-      buttonStart.classList.remove('isActive');
+      refs.buttonStart.classList.remove('isActive');
 
-      iziToast.show({
-        message: 'Please choose a date in the future',
-        messageColor: '#ffffff',
-        backgroundColor: '#ff0000',
-        icon: 'fa-regular fa-circle-xmark',
-        iconColor: '#a22b2b',
-        position: 'topRight',
-      });
+      iziToastOptions();
+    } else {
+      // Вешаем слушатель события на кнопку Start
+      refs.buttonStart.addEventListener('click', onClick);
+      refs.buttonStart.classList.add('isActive');
     }
-
     userSelectedDate = selectedDates[0];
-    buttonStart.classList.add('isActive');
 
     console.log(userSelectedDate);
   },
 };
 
+// Опции подключенного через библиотеку alert
+function iziToastOptions() {
+  return iziToast.show({
+    message: 'Please choose a date in the future',
+    messageColor: '#ffffff',
+    backgroundColor: '#ff0000',
+    icon: 'fa-regular fa-circle-xmark',
+    iconColor: '#a22b2b',
+    position: 'topRight',
+  });
+}
+
+// Сохраняем в переменную результат вызова экземпляра flatpickr
 const fp = flatpickr('#datetime-picker', options);
 
+// Функция обработчика слушателя событий кнопки Start
 function onClick() {
+  refs.buttonStart.removeEventListener('click', onClick);
+  refs.buttonStart.classList.remove('isActive');
+  fp.destroy();
+  // document.getElementById('#datetime-picker').disabled = true;
+
   return setInterval(() => {
     const timeLeft = convertMs(userSelectedDate - new Date());
-    fieldDays.textContent = addLeadingZero(timeLeft.days);
-    fieldHours.textContent = addLeadingZero(timeLeft.hours);
-    fieldMinutes.textContent = addLeadingZero(timeLeft.minutes);
-    fieldSeconds.textContent = addLeadingZero(timeLeft.seconds);
+    refs.fieldDays.textContent = addLeadingZero(timeLeft.days);
+    refs.fieldHours.textContent = addLeadingZero(timeLeft.hours);
+    refs.fieldMinutes.textContent = addLeadingZero(timeLeft.minutes);
+    refs.fieldSeconds.textContent = addLeadingZero(timeLeft.seconds);
   }, 1000);
 }
 
@@ -58,21 +74,21 @@ function onClick() {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
+// Функция для подсчёта значения времени, где ms разница между конечной и текущей датой в миллисекундах
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+  // Количество миллисекунд в единице времени
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
+  // Оставшиеся дни
   const days = Math.floor(ms / day);
-  // Remaining hours
+  // Оставшиеся часы
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
+  // Оставшиеся минуты
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+  // Оставшиеся секунды
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
