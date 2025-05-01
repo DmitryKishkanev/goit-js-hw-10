fa4c5be9749ef210f9b008148d7bffa0b16c132b
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-05-02 02:37:26 +0300

    создал промис внутри onFormSubmit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,69 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import '../css/2-snackbar.css';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayField: document.querySelector('input[name="delay"]'),
+  //   submitButton: document.querySelector('button[type="submit"]'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+// refs.submitButton.addEventListener('click', onSubmitClick);
+
+// function onSubmitClick() {
+//   makePromise(Number(refs.delayField.value))
+//     .then(message => {
+//       iziToastOptions(
+//         `✅ Fulfilled promise in ${refs.delayField.value}ms`,
+//         'green'
+//       );
+//     })
+//     .catch(message => {
+//       iziToastOptions(
+//         `❌ Rejected promise in ${refs.delayField.value}ms`,
+//         'red'
+//       );
+//     });
+// }
+
+function makePromise(delay) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (refs.formEl.elements['state'].value === 'fulfilled') {
+        res(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        rej(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  makePromise(Number(refs.delayField.value))
+    .then(message => {
+      iziToastOptions(
+        `✅ Fulfilled promise in ${refs.delayField.value}ms`,
+        'green'
+      );
+    })
+    .catch(message => {
+      iziToastOptions(
+        `❌ Rejected promise in ${refs.delayField.value}ms`,
+        'red'
+      );
+    });
+}
+
+function iziToastOptions(message, backgroundColor) {
+  return iziToast.show({
+    message,
+    messageColor: '#ffffff',
+    backgroundColor,
+    icon: 'fa-regular fa-circle-xmark',
+    iconColor: '#a22b2b',
+    position: 'topRight',
+  });
+}
