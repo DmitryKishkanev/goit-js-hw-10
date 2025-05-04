218e88938d27de3553e6c65fef2b929834564c5b
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-05-04 22:39:29 +0300

    добавил кнопку Clear

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,17 +5,44 @@ import '../css/2-snackbar.css';
 const refs = {
   formEl: document.querySelector('.form'),
   delayField: document.querySelector('input[name="delay"]'),
-  // formButton: document.querySelector('button[type="submit"]'),
+  stateField: document.querySelectorAll('input[name="state"]'),
+  formCreateButton: document.querySelector('button[type="submit"]'),
+  formClearButton: document.querySelector('button[type="button"]'),
 };
 
-// Вешаем на форму слушатель события
+// Вешаем слушатели события
 refs.formEl.addEventListener('submit', onFormSubmit);
+refs.delayField.addEventListener('input', checkFields);
+refs.formClearButton.addEventListener('click', onformClearButton);
+refs.stateField.forEach(radio => {
+  radio.addEventListener('change', checkFields);
+});
+
+function checkFields() {
+  const selectedRadio = document.querySelector('input[name="state"]:checked');
+
+  if (selectedRadio && refs.delayField.value !== '') {
+    refs.formCreateButton.classList.add('isActive');
+    refs.formClearButton.classList.add('isActive');
+  } else {
+    refs.formCreateButton.classList.remove('isActive');
+    refs.formClearButton.classList.remove('isActive');
+  }
+}
+
+function onformClearButton() {
+  refs.formEl.reset();
+  refs.formCreateButton.classList.remove('isActive');
+  refs.formClearButton.classList.remove('isActive');
+}
 
 // Функция создания промиса в аргумены, которой передаём delay - время задержки setTimeout
 function makePromise(delay) {
+  const selectedRadio = document.querySelector('input[name="state"]:checked');
+
   return new Promise((res, rej) => {
     setTimeout(() => {
-      if (refs.formEl.elements['state'].value === 'fulfilled') {
+      if (selectedRadio.value === 'fulfilled') {
         res(`✅ Fulfilled promise in ${delay}ms`);
       } else {
         rej(`❌ Rejected promise in ${delay}ms`);
@@ -44,6 +71,8 @@ function onFormSubmit(evt) {
     })
     .finally(() => {
       refs.formEl.reset();
+      refs.formCreateButton.classList.remove('isActive');
+      refs.formClearButton.classList.remove('isActive');
     });
 }
 
