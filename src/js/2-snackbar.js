5591d96c40a15a4d813b174d7de10e8a738474fb
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-05-06 23:18:20 +0300

    добавил в snackbar сохранение данных в localStorage

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,45 +4,62 @@ import '../css/2-snackbar.css';
 
 const refs = {
   formEl: document.querySelector('.form'),
-  delayField: document.querySelector('input[name="delay"]'),
-  stateField: document.querySelectorAll('input[name="state"]'),
   formCreateButton: document.querySelector('button[type="submit"]'),
   formClearButton: document.querySelector('button[type="button"]'),
 };
 
+// Переменная для хранения localStorage ключа
+const STORAGE_KEY = 'feedback-form-state';
+
+// Создаём объект с пустыми свойствами для записи данных и сохранения его в localStorage
+const formData = {
+  delay: '',
+  state: '',
+};
+
 // Вешаем слушатели события
+refs.formEl.addEventListener('input', onInput);
 refs.formEl.addEventListener('submit', onFormSubmit);
-refs.delayField.addEventListener('input', checkFields);
-refs.formClearButton.addEventListener('click', onformClearButton);
-refs.stateField.forEach(radio => {
-  radio.addEventListener('change', checkFields);
-});
+refs.formClearButton.addEventListener('click', onFormClearButton);
 
-function checkFields() {
-  const selectedRadio = document.querySelector('input[name="state"]:checked');
+// Вызов функции записи данных в форму из localStorage и передача ему аргументом нашей формы
+populateForm(refs.formEl);
 
-  if (selectedRadio && refs.delayField.value !== '') {
+function onInput(evt) {
+  const { name, value } = evt.target;
+  formData[name] = value;
+
+  // Проверяем, есть ли хотябы одно поле, значение которого не пустое
+  const hasData = Object.values(formData).some(field => field !== '');
+
+  if (hasData) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
     refs.formCreateButton.classList.add('isActive');
     refs.formClearButton.classList.add('isActive');
   } else {
+    localStorage.removeItem(STORAGE_KEY);
+
     refs.formCreateButton.classList.remove('isActive');
     refs.formClearButton.classList.remove('isActive');
+    return;
   }
 }
 
-function onformClearButton() {
+function onFormClearButton() {
+  localStorage.removeItem(STORAGE_KEY);
   refs.formEl.reset();
+  formData.delay = '';
+  formData.state = '';
   refs.formCreateButton.classList.remove('isActive');
   refs.formClearButton.classList.remove('isActive');
 }
 
 // Функция создания промиса в аргумены, которой передаём delay - время задержки setTimeout
 function makePromise(delay) {
-  const selectedRadio = document.querySelector('input[name="state"]:checked');
-
   return new Promise((res, rej) => {
     setTimeout(() => {
-      if (selectedRadio.value === 'fulfilled') {
+      if (formData.state === 'fulfilled') {
         res(`✅ Fulfilled promise in ${delay}ms`);
       } else {
         rej(`❌ Rejected promise in ${delay}ms`);
@@ -56,24 +73,38 @@ function onFormSubmit(evt) {
   evt.preventDefault();
 
   // Вызов функции промиса
-  makePromise(Number(refs.delayField.value))
+  makePromise(Number(formData.delay))
     .then(message => {
-      iziToastOptions(
-        `✅ Fulfilled promise in ${refs.delayField.value}ms`,
-        'green'
-      );
+      iziToastOptions(`✅ Fulfilled promise in ${formData.delay}ms`, 'green');
     })
     .catch(message => {
-      iziToastOptions(
-        `❌ Rejected promise in ${refs.delayField.value}ms`,
-        'red'
-      );
+      iziToastOptions(`❌ Rejected promise in ${formData.delay}ms`, 'red');
     })
     .finally(() => {
+      localStorage.removeItem(STORAGE_KEY);
       refs.formEl.reset();
+      formData.delay = '';
+      formData.state = '';
       refs.formCreateButton.classList.remove('isActive');
       refs.formClearButton.classList.remove('isActive');
     });
+}
+
+// Функция записи данных в форму из localStorage
+function populateForm(form) {
+  const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedForm) {
+    Object.keys(savedForm).forEach(key => {
+      if (form.elements[key]) {
+        form.elements[key].value = savedForm[key];
+        formData[key] = savedForm[key];
+      }
+    });
+
+    refs.formCreateButton.classList.add('isActive');
+    refs.formClearButton.classList.add('isActive');
+  }
 }
 
 // Опции подключенного через библиотеку alert
